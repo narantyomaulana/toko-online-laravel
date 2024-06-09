176cd53906310b96a0531dbdd00ebf9f25948b8c
narantyo maulana <olandeoxys@gmail.com>
2024-06-09 18:21:07 +0700

    change folder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,12 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
@@ -17,6 +19,19 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->longText('address_one');
+            $table->longText('address_two');
+            $table->integer('provinces_id');
+            $table->integer('regencies_id');
+            $table->integer('zip_code');
+            $table->string('country');
+            $table->string('phone_number');
+            $table->string('store_name');
+            $table->integer('categories_id');
+            $table->integer('store_status');
+
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
@@ -24,9 +39,11 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
-};
+}
--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,15 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('register/check', 'Auth\RegisterController@check')->name('api-register-check');
+Route::get('provinces', 'API\LocationController@provinces')->name('api-provinces');
+Route::get('regencies/{provinces_id}', 'API\LocationController@regencies')->name('api-regencies');
